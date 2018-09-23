d923a2478ebdf7886e3658509f2466a522bcae35
peterstaylor <peters.taylor@gmail.com>
2018-09-23 16:01:30 -0400

    working on a better sorting function

---
+++ final_sort.js
@@ -0,0 +1,12 @@
+function final_sort() {
+  var tracker = SpreadsheetApp.openById('1isEdFurIx497X4XgrO5PDwA45wpc3Jo_psnS8MZmsi8').getSheets()[0]; 
+  var last_row = tracker.getLastRow(); 
+  var last_column = tracker.getLastColumn(); 
+  
+  var point = 3;     // we start looking at row three
+  
+  while (point <= last_row){
+	tracker.getRange(point, 
+  
+  }
+}
