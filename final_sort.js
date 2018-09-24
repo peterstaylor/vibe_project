b438ff814fbd63d0c7efa01cdcb08ad2c2054eac
peterstaylor <peters.taylor@gmail.com>
2018-09-23 20:32:14 -0400

    clock out: fixed the sorting of different clients with teh same last name

--- final_sort.js
+++ final_sort.js
@@ -4,28 +4,8 @@ function final_sort() {
   var last_column = tracker.getLastColumn(); 
   
   // first step is to just sort everything by the client last name
+  tracker.getRange(3, 1, last_row, last_column).sort({ column: 3, ascending: true }); 
   tracker.getRange(3, 1, last_row, last_column).sort({ column: 1, ascending: true }); 
 
-  // next we need to solve the problem of different clients with the same last name
-  
-  var names = tracker.getRange(3, 1, last_row, 3).getValues(); 
-  
-  var point = 0;     // we start looking at the first row of data 
-  
- while (point < names.length){
-	
-	
-	for(var spread = 1; names[point][0] == names[point+spread][0] ; spread++){
-		
-	}
-	if (spread == 1){
-		point = point + 1; 
-	}
-	else{
-		tracker.getRange(point+3,1, point+3+spread-1, last_column).sort({ column: 3, ascending: true });
-		point = point + spread; 
-	}
-  }
-  
-  Logger.log(names[3][0]); 
+
 }
